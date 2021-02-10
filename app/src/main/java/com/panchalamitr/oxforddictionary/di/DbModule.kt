ed636992a4fb4e3746abc5b73b86b7da125f01c7
Amit Panchal <panchalamitr0@gmail.com>
2021-02-10 14:03:58 +0800

    Update Repositoy
    Update ApplicationComponents to SingletonComponent as require while updating repository

--- app/src/main/java/com/panchalamitr/oxforddictionary/di/DbModule.kt
+++ app/src/main/java/com/panchalamitr/oxforddictionary/di/DbModule.kt
@@ -6,12 +6,12 @@ import com.panchalamitr.oxforddictionary.data.local.MovieDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 class DbModule {
 
     @Provides
--- app/src/main/java/com/panchalamitr/oxforddictionary/di/RetrofitModule.kt
+++ app/src/main/java/com/panchalamitr/oxforddictionary/di/RetrofitModule.kt
@@ -9,14 +9,14 @@ import com.panchalamitr.oxforddictionary.util.Constant
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 class RetrofitModule {
 
     @Singleton
